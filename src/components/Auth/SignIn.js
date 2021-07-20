58fef38bbbca8bacc85f60d3af830440f608f3a0
Manjunath <amanjunathmanju4@gmail.com>
2021-07-20 12:56:07 +0530

    added auth

--- src/components/Auth/SignIn.js
+++ src/components/Auth/SignIn.js
@@ -1,14 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Form, FormGroup, Label, Input, Button, Card, CardTitle, CardBody, Row, Col } from 'reactstrap'
 
 function SignIn() {
+
+const [signinCred, setsigninCred] = useState({
+    email: '', 
+    password: '', 
+})
+
+const submitHandler = () => {
+    
+}
+
     return (
         <Row className= "d-flex align-items-center py-5">
             <Col sm= {{ size:"12" }} md= {{ size:"4", offset: 4}}> 
         <Card  body className= "shadow p-3 mb-5 bg-white rounded">            
             <CardBody>
             <CardTitle><h2>Sign In</h2></CardTitle>
-            <Form className="form">
+            <Form className="form" onSubmit= {() => submitHandler()}>
             <FormGroup>
                 <Label for="exampleEmail">Email</Label>
                 <Input
@@ -16,6 +26,9 @@ function SignIn() {
                 name="email"
                 id="exampleEmail"
                 placeholder="example@example.com"
+                value= {signinCred.email}
+                onChange= {e => setsigninCred({...signinCred, email: e.target.value})}
+                required
                 />
             </FormGroup>
             <FormGroup>
@@ -25,9 +38,12 @@ function SignIn() {
                 name="password"
                 id="examplePassword"
                 placeholder="********"
+                value= {signinCred.password}
+                onChange= {e => setsigninCred({...signinCred, password: e.target.value})}
+                required
                 />
             </FormGroup>
-            <Button className= "pull-right" color= "primary" size= "lg">Sign In</Button>
+            <Button className= "pull-right" color= "primary" size= "lg" type= "submit">Sign In</Button>
         </Form>
         </CardBody>
         <p className= "pull-right d-flex text-muted">Don't have an account ? < a href= '/signup'><span> Sign Up</span></a></p>
